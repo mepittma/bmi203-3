7b58fcd1085b0787455aa9450381adb3301ce35e
Maureen Pittman <student@udp193667uds.ucsf.edu>
2018-02-18 11:30:18 -0800

    reading in proteins works

--- align/__main__.py
+++ align/__main__.py
@@ -1,11 +1,22 @@
 # this file is only called when the package is called from the command
 # line
-from .algs import read_blosum#, scoring_mat, traceback
+from .algs import read_blosum, read_prot#, scoring_mat, traceback
 
+# Read in substitution matrix
 filepath = "/Users/student/Documents/BMI206/bmi203-3/HW3_due_02_23/BLOSUM50"
-#basename = os.path.basename(filepath)
-#name = os.path.splitext(basename)
+blosum = read_blosum(filepath)
 
+# Read in protein sequence
+filepath = "/Users/student/Documents/BMI206/bmi203-3/HW3_due_02_23/sequences/prot-0004.fa"
+seq_m = read_prot(filepath)
+print(seq_m)
 
-df = read_blosum(filepath)
-print(df)
+filepath = "/Users/student/Documents/BMI206/bmi203-3/HW3_due_02_23/sequences/prot-0008.fa"
+seq_n = read_prot(filepath)
+print(seq_n)
+
+# Initialize empty matrices
+#score_mat = [[None]*len(seq_n) for _ in range(0,len(seq_m))]
+#state_mat = [[None]*len(seq_n) for _ in range(0,len(seq_m))]
+#print(len(score_mat))
+#print(len(score_mat[1]))
--- align/algs.py
+++ align/algs.py
@@ -21,9 +21,37 @@ def read_blosum(filepath):
 
     return data
 
+def read_prot(filepath):
+    """
+    This function accepts the filepath of a protein to align, ignores the first line
+    (proceeded by '>' char), strips newlines, and returns the protein as a single string.
+    """
+    seq = ""
+    with open(filepath) as f:
+        for line in f:
+            if not line.startswith(">"):
+                seq += (line.rstrip())
 
-def scoring_mat(blosum, seq_n, seq_m, score_mat, state_mat):
+    return seq
+
+def scoring_mat(blosum, seq_m, seq_n):
     """
     This function fills in a scoring matrix for two sequences to align, as well
-    as a matrix that stores the "up", "left", or "right" state of each
+    as a matrix that stores the previous state of that score (whether it was
+    derived from up, left, or diagonal state).
+
+    Adapted from pseudocode here: https://github.com/elizabethfong/SparkSmithWaterman/wiki/Smith-Waterman-Algorithm
+
+    Inputs:
+        blosum: the substitution matrix being used
+        seq_n, seq_m: sequences to align
+    Output:
+        score_mat: matrix containing maximum alignment scores for each cell
+        state_mat: matrix containing direction from which maximum was derived
     """
+
+    # Initialize empty matrices
+    score_mat = [[None]*len(seq_n) for _ in range(0,len(seq_m))]
+    state_mat = [[None]*len(seq_n) for _ in range(0,len(seq_m))]
+    print(len(score_mat))
+    print
